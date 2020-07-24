d8eefcf534363b78cae754b1393451cfb3cf9e3f
Divine Uchechukwu Linus <divine19@fb.com>
2020-07-24 04:38:11 -0400

    Camera Application

--- Men Bodyzone/CamsViewController.m
+++ Men Bodyzone/CamsViewController.m
@@ -39,6 +39,10 @@
 
 - (IBAction)choosePhoto:(id)sender {
     
+    picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    [picker setSourceType:UIImagePickerControllerSourceTypePhotoLibrary];
+    [self presentViewController:picker animated:YES completion:NULL];
 }
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<UIImagePickerControllerInfoKey,id> *)info{
     
