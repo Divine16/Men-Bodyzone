d5dd4097ade5ad322d545a159b6cfe153a063a5c
Divine Uchechukwu Linus <divine19@fb.com>
2020-07-24 04:00:22 -0400

    Camera Application

---
+++ Men Bodyzone/MapViewController.h
@@ -0,0 +1,17 @@
+//
+//  MapViewController.h
+//  Men Bodyzone
+//
+//  Created by Divine Uchechukwu Linus on 7/24/20.
+//  Copyright © 2020 Divine Uchechukwu Linus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MapViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Men Bodyzone/MapViewController.m
@@ -0,0 +1,32 @@
+//
+//  MapViewController.m
+//  Men Bodyzone
+//
+//  Created by Divine Uchechukwu Linus on 7/24/20.
+//  Copyright © 2020 Divine Uchechukwu Linus. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@interface MapViewController ()
+
+@end
+
+@implementation MapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
